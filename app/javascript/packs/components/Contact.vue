58780d46e4d5cfb4410732a29939360f5b1df719
harapeko-wolf <harapeko.inc@gmail.com>
2019-02-16 20:52:24 +0900

    checkbox layout 整備

--- app/javascript/packs/components/Contact.vue
+++ app/javascript/packs/components/Contact.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <!-- 内容はお好みで -->
-    <p>If you want to contact me, you send mail to below address.</p>
-    <p>sosadzac@gmail.com</p>
+    <h2 class="title">If you want to contact me, you send mail to below address.</h2>
+    <p>
+      <i class="material-icons mail">contact_mail</i>sosadzac@gmail.com
+    </p>
   </div>
 </template>
+<style lang="scss" scoped>
+.title {
+  font-size: 2rem;
+}
+.mail {
+  display: block;
+}
+</style>
+
--- app/javascript/packs/components/Index.vue
+++ app/javascript/packs/components/Index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div　class="task">
     <!-- 新規作成部分 -->
     <div class="row">
       <div class="col s10 m11">
         <input class="form-control" placeholder="Add your task!!">
       </div>
       <div class="col s2 m1">
-        <div class="btn-floating waves-effect waves-light red">
+        <div class="btn-floating waves-effect waves-light">
           <i class="material-icons">add</i>
         </div>
       </div>
@@ -15,16 +15,22 @@
     <div>
       <ul class="collection">
         <li id="row_task_1" class="collection-item">
-          <input type="checkbox" id="task_1">
-          <label for="task_1">Sample Task</label>
+          <label>
+            <input type="checkbox" class="filled-in" id="task_1">
+            <span for="task_1">Sample Task</span>
+          </label>
         </li>
         <li id="row_task_2" class="collection-item">
-          <input type="checkbox" id="task_2">
-          <label for="task_2">Sample Task</label>
+          <label>
+            <input type="checkbox" class="filled-in" id="task_2">
+            <span for="task_2">Sample Task</span>
+          </label>
         </li>
         <li id="row_task_3" class="collection-item">
-          <input type="checkbox" id="task_3">
-          <label for="task_3">Sample Task</label>
+          <label>
+            <input type="checkbox" class="filled-in" id="task_3">
+            <span for="task_3">Sample Task</span>
+          </label>
         </li>
       </ul>
     </div>
@@ -34,14 +40,30 @@
     <div id="finished-tasks" class="display_none">
       <ul class="collection">
         <li id="row_task_4" class="collection-item">
-          <input type="checkbox" id="'task_4" checked="checked">
-          <label v-bind:for="task_4" class="line-through">Done Task</label>
+          <label>
+            <input type="checkbox" class="filled-in" id="'task_4" checked="checked">
+            <span v-bind:for="task_4" class="line-through">Done Task</span>
+          </label>
         </li>
         <li id="row_task_5" class="collection-item">
-          <input type="checkbox" id="'task_5" checked="checked">
-          <label v-bind:for="task_5" class="line-through">Done Task</label>
+          <label>
+            <input type="checkbox" class="filled-in" id="'task_5" checked="checked">
+            <span v-bind:for="task_5" class="line-through">Done Task</span>
+          </label>
         </li>
       </ul>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.task {
+  margin-top: 40px;
+}
+.collection {
+  margin: 15px 0 30px 0;
+}
+label > span {
+  display: inherit !important;
+}
+</style>
