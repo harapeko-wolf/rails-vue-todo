6d4f608043ea64605a68e9c5bec4d3c7cac7591e
harapeko-wolf <harapeko.inc@gmail.com>
2019-03-15 02:23:18 +0900

    memo

--- app/javascript/packs/components/Index.vue
+++ app/javascript/packs/components/Index.vue
@@ -3,7 +3,12 @@
     <!-- 新規作成部分 -->
     <div class="row">
       <div class="col s10 m11">
-        <input v-model="newTask" @keyup.enter="createTask" class="form-control" placeholder="Add your task!!" />
+        <input
+          v-model="newTask"
+          @keyup.enter="createTask"
+          class="form-control"
+          placeholder="Add your task!!"
+        >
       </div>
       <div class="col s2 m1">
         <div @click="createTask" class="btn-floating waves-effect waves-light">
@@ -14,14 +19,23 @@
     <!-- リスト表示部分 -->
     <div>
       <ul class="collection">
-        <li v-for="task in tasks" v-if="!task.is_done" v-bind:id="'row_task_' + task.id" class="collection-item">
+        <li
+          v-for="task in tasks"
+          v-if="!task.is_done"
+          v-bind:id="'row_task_' + task.id"
+          class="collection-item"
+        >
           <label>
-            <input type="checkbox" @change="doneTask(task.id)" class="filled-in" v-bind:id="'task_' + task.id">
+            <input
+              type="checkbox"
+              @change="doneTask(task.id)"
+              class="filled-in"
+              v-bind:id="'task_' + task.id"
+            >
             <span v-bind:for="'task_' + task.id">{{ task.name }}</span>
           </label>
           <button @click="deleteTask(task.id)" class="btn-small red button-location">Delete</button>
         </li>
-
       </ul>
     </div>
     <!-- 完了済みタスク表示ボタン -->
@@ -29,9 +43,19 @@
     <!-- 完了済みタスク一覧 -->
     <div id="finished-tasks" class="display_none">
       <ul class="collection">
-        <li v-for="task in tasks" v-if="task.is_done" v-bind:id="'row_task_' + task.id" class="collection-item">
+        <li
+          v-for="task in tasks"
+          v-if="task.is_done"
+          v-bind:id="'row_task_' + task.id"
+          class="collection-item"
+        >
           <label>
-            <input type="checkbox" class="filled-in" v-bind:id="'task_' + task.id" checked="checked">
+            <input
+              type="checkbox"
+              class="filled-in"
+              v-bind:id="'task_' + task.id"
+              checked="checked"
+            >
             <span v-bind:for="'task_' + task.id" class="line-through">{{ task.name }}</span>
           </label>
           <button @click="deleteTask(task.id)" class="btn-small red button-location">Delete</button>
@@ -45,8 +69,10 @@
 import axios from "axios";
 
 axios.defaults.headers.common = {
-    'X-Requested-With': 'XMLHttpRequest',
-    'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+  "X-Requested-With": "XMLHttpRequest",
+  "X-CSRF-TOKEN": document
+    .querySelector('meta[name="csrf-token"]')
+    .getAttribute("content")
 };
 
 export default {
@@ -60,50 +86,66 @@ export default {
     this.fetchTasks();
   },
   methods: {
-    createTask: function(){
-      if(!this.newTask) return;
+    createTask: function() {
+      if (!this.newTask) return;
 
-      axios.post('/api/tasks', { task: { name: this.newTask } }).then((res) => {
-        this.tasks.unshift(res.data.task)
-        this.newTask = '';
-      }, (error) => {
-        console.log(error);
-      })
+      axios.post("/api/tasks", { task: { name: this.newTask } }).then(
+        res => {
+          this.tasks.unshift(res.data.task);
+          this.newTask = "";
+        },
+        error => {
+          console.log(error);
+        }
+      );
     },
-    fetchTasks: function () {
-      axios.get('/api/tasks').then((res) => {
-        // for(var i = 0; i < res.data.tasks.length; i++) {
-        //   this.tasks.push(res.data.tasks[i]);
-        // }
-        const tasks = res.data.tasks.map((task) => { return task });
-        this.tasks = tasks;
-      }, (error) => {
-        console.log(error);
-      });
+    fetchTasks: function() {
+      axios.get("/api/tasks").then(
+        res => {
+          // for(var i = 0; i < res.data.tasks.length; i++) {
+          //   this.tasks.push(res.data.tasks[i]);
+          // }
+          const tasks = res.data.tasks.map(task => {
+            return task;
+          });
+          this.tasks = tasks;
+        },
+        error => {
+          console.log(error);
+        }
+      );
     },
-    doneTask: function(task_id){
-      axios.put('/api/tasks/' + task_id, { task: { is_done: 1 } }).then((res) => {
-        this.fetchTasks();
-        // this.moveFinishedTask(task_id)
-      }, (error) => {
-        console.log(error)
-      })
+    doneTask: function(task_id) {
+      axios.put("/api/tasks/" + task_id, { task: { is_done: 1 } }).then(
+        res => {
+          this.fetchTasks();
+          // this.moveFinishedTask(task_id)
+        },
+        error => {
+          console.log(error);
+        }
+      );
     },
-    deleteTask: function(task_id){
-      axios.delete('/api/tasks/' + task_id).then((res) => {
-        const tasks = this.tasks.filter((task) => { return task.id !== task_id });
-        this.tasks = tasks;
-      }, (error) => {
-        console.log(error)
-      })
+    deleteTask: function(task_id) {
+      axios.delete("/api/tasks/" + task_id).then(
+        res => {
+          const tasks = this.tasks.filter(task => {
+            return task.id !== task_id;
+          });
+          this.tasks = tasks;
+        },
+        error => {
+          console.log(error);
+        }
+      );
     },
-    displayFinishedTasks: function(){
-      document.querySelector('#finished-tasks').classList.toggle('display_none');
+    displayFinishedTasks: function() {
+      document
+        .querySelector("#finished-tasks")
+        .classList.toggle("display_none");
     }
   }
 };
-
-
 </script>
 
 <style lang="scss" scoped>
